4e382f5ed1052b119619e0b8f05c6e18e9b0583e
Emilia-Esther-Jurado-Quiroz <72291524+Emilia-Esther-Jurado-Quiroz@users.noreply.github.com>
2020-10-17 20:35:41 -0500

    Add files via upload

---
+++ python/tarea.py
@@ -0,0 +1,29 @@
+list = ['', '', 19978, 2000]
+print ("Cp: #", list[2])
+
+list[2] = 5555231345
+print ("Telefono fijo:(55)",list[2])
+
+list = ['', '', 5519978456,]
+print ("Telefono de celular:(044)",list[2])
+
+list[2] = "Santa Anta Tlacotenco,La lupita Xolco"
+print ("Direccion:", list[2])
+
+list[2] ="Emilia Esther"
+print ("Nombre:", list[2])
+
+list[2] = "171190089@milpatalta.tec.mx"
+print ("Correo Institucional:",list[2])
+
+list[2] ="Dia: 14/ Mes: 10/ Año:2020"
+print ("Fecah: ", list[2])
+
+list[2] =171190089
+print ("No. De control : ", list[2])
+
+list = ['', '',21,21]
+print ("Edad:21", list[1])
+
+
+
